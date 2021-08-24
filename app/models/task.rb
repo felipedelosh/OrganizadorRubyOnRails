f74c79210f7edbdc98b8b4ebf9d88dabbddf0881
Andres Felipe Herndandez <38327255+felipedelosh@users.noreply.github.com>
2021-08-24 16:20:16 -0500

    Generate a tasks with multi participants

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'
+  has_many :participating_user, class_name: 'Participant'
+  has_many :participants, through: :participating_user, source: :user
 
   validates :name, :description, presence: true
   validate :due_data_validity
