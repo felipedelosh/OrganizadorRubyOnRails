c78ba2ffadd6ce528691f58f770ec9b7ecc87e69
Andres Felipe Herndandez <38327255+felipedelosh@users.noreply.github.com>
2021-08-24 21:40:26 -0500

    cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, Task
+    can :manage, Task, owner_id: user.id #xq task_controller contiene @owner = current user
   end
 end
